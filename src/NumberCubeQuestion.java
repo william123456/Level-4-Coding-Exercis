9ff27bbdded587cd23838354ab180be20f494832
LEAUGE COMPUTER <DO NOT CHANGE THIS/>
2017-01-12 19:59:49 -0800

    HeyKiddoIUpdatedit
    YAY ALGORITHIMS’nSTUfF

---
+++ src/NumberCubeQuestion.java
@@ -0,0 +1,28 @@
+
+public class NumberCubeQuestion {
+
+	public static int[] getCubeTosses(NumberCube nC, int arrayLength) {
+
+		int[] cubeValueArray = new int[arrayLength];
+
+		for (int i = 0; i < arrayLength; i++) {
+			cubeValueArray[i] = nC.toss();
+
+		}
+
+		return cubeValueArray;
+
+	}
+
+	public static int getLongestRun(int[] runArray) {
+		int LongestRunIndex;
+		int currentRun;
+		int greatesetRun;
+		
+		for (int x = 0; x < runArray.length; x++) {
+		if
+			
+		}
+
+	}
+}
